272a8f1284f7e798f17f171e57b054d58cea9398
Samatar Xasan <Samatar.Xasan@gmail.com>
2020-12-08 16:48:32 +0000

    Comments in the JS and CSS file

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
+
+// Events alter the way page looks by adding a new class to the container which is the parent
+// of the left and right section
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
